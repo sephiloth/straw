3c6bdebfc9ad9b3c387003f9197a75f1b0412592
yuki <tutsujiiro0121metro_my@ezweb.ne.jp>
2016-02-06 10:49:29 +0000

    add message

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206101442) do
+ActiveRecord::Schema.define(version: 20160206104728) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 20160206101442) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
