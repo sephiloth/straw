296af6f2a322ec55cf3b4cb126973ac9693c4ce2
yuki <tutsujiiro0121metro_my@ezweb.ne.jp>
2016-02-06 10:20:02 +0000

    add items

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :items
   devise_for :authors
-  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206092754) do
+ActiveRecord::Schema.define(version: 20160206101442) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -37,5 +37,13 @@ ActiveRecord::Schema.define(version: 20160206092754) do
 
   add_index "authors", ["email"], name: "index_authors_on_email", unique: true
   add_index "authors", ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
